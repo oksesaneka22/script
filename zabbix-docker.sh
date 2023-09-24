f458998df9920d77d9fcc32faf40a64f5f44dbb9
oksesaneka22 <49028843+oksesaneka22@users.noreply.github.com>
2023-09-24 17:32:57 +0300

    Update zabbix-docker.sh

--- zabbix-docker.sh
+++ zabbix-docker.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
-for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+sudo apt update
+sudo apt install apt-transport-https
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install docker-ce
 
-sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg -y
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+sudo usermod -aG docker $USER
 
-echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo docker run hello-world
+sudo systemctl status docker
+docker -v
 
 mkdir /var/lib/zabbix/
 cd /var/lib/zabbix/

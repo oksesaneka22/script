c4089ed0b88b51a9ea8e67be6ede713b4152f5f3
oksesaneka22 <49028843+oksesaneka22@users.noreply.github.com>
2023-09-24 17:47:13 +0300

    Update zabbix-docker.sh

--- zabbix-docker.sh
+++ zabbix-docker.sh
@@ -21,30 +21,26 @@ sudo docker network create zabbix-net
 
 sudo docker run -d \
 --name zabbix-server \
+-p 80:8080 -p 443:8443 \
 --network zabbix-net \
+-e DB_SERVER_HOST="zabbix-server" \
+-e ZBX_SERVER_HOST="zabbix-server" \
+-e PHP_TZ="Europe/Kiev" \
+-v /var/lib/zabbix/timezone:/etc/timezone \
 -v /var/lib/zabbix/timezone:/etc/timezone \
 -v /var/lib/zabbix/localtime:/etc/localtime \
--e POSTGRES_PASSWORD=zabbix \
--e POSTGRES_USER=zabbix \
--d postgres:alpine
+-v /var/lib/zabbix/localtime:/etc/localtime \
 -v /var/lib/zabbix/alertscripts:/usr/lib/zabbix/alertscripts \
 -v /var/lib/zabbix/timezone:/etc/timezone \
 -v /var/lib/zabbix/localtime:/etc/localtime \
 -p 10051:10051 -e DB_SERVER_HOST="zabbix-postgres" \
--e POSTGRES_USER="zabbix" \
--e POSTGRES_PASSWORD="zabbix" \
+-e POSTGRES_PASSWORD=zabbix \
+-e POSTGRES_USER=zabbix \
+-d postgres:alpine
 -d zabbix/zabbix-server-pgsql:alpine-latest
--p 80:8080 -p 443:8443 \
--e DB_SERVER_HOST="zabbix-postgres" \
--v /var/lib/zabbix/timezone:/etc/timezone \
--v /var/lib/zabbix/localtime:/etc/localtime \
--e POSTGRES_USER="zabbix" \
--e POSTGRES_PASSWORD="zabbix" \
--e ZBX_SERVER_HOST="zabbix-server" \
--e PHP_TZ="Europe/Kiev" \
 -d zabbix/zabbix-web-nginx-pgsql:alpine-latest
 
 
-sudo docker start zabbix-postgres
-sudo docker start zabbix-server
+
+
 sudo docker start zabbix-web

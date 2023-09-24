6cc257270a935440c8352b498f764d2aad18f442
oksesaneka22 <49028843+oksesaneka22@users.noreply.github.com>
2023-09-24 17:30:34 +0300

    Update zabbix-docker.sh

--- zabbix-docker.sh
+++ zabbix-docker.sh
@@ -14,7 +14,7 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo docker run hello-world
 
 mkdir /var/lib/zabbix/

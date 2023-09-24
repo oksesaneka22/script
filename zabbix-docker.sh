a4485b66bfd46072184772705222f37b26bcda19
oksesaneka22 <49028843+oksesaneka22@users.noreply.github.com>
2023-09-24 17:29:40 +0300

    Update zabbix-docker.sh

--- zabbix-docker.sh
+++ zabbix-docker.sh
@@ -5,7 +5,7 @@ for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do
 sudo apt-get update
 sudo apt-get install ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg -y
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
 echo \

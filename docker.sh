9d9505c2984485282d0058a5b2eb89e186471948
oksesaneka22 <49028843+oksesaneka22@users.noreply.github.com>
2023-10-08 14:04:28 +0300

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install apt-transport-https
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install docker-ce
+
+sudo usermod -aG docker $USER
+
+sudo systemctl status docker
+docker -v

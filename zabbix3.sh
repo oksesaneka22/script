14d285e451a2d17b669c340867049428a17f8d15
oksesaneka22 <49028843+oksesaneka22@users.noreply.github.com>
2023-10-15 17:35:46 +0300

    Update zabbix3.sh

--- zabbix3.sh
+++ zabbix3.sh
@@ -1,2 +1,2 @@
  wget https://oksesaneka22.github.io/script/docker.sh && bash docker.sh 
- wget https://oksesaneka22.github.io/script/docker-compose.yml && docker-compose up -d
+ wget https://oksesaneka22.github.io/script/docker-compose.yaml && docker-compose up -d

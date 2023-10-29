c99946d5939c30118d16325dfa1e7516e809fcb6
oksesaneka22 <49028843+oksesaneka22@users.noreply.github.com>
2023-10-29 14:15:17 +0200

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -5,7 +5,7 @@ sudo apt install apt-transport-https
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update
-sudo apt install docker-ce
+sudo apt install docker-ce -y
 
 sudo usermod -aG docker $USER
 

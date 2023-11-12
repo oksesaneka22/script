11e634628608fe7c2a7b5d5f37436874eadbe1fa
oksesaneka22 <49028843+oksesaneka22@users.noreply.github.com>
2023-11-12 14:45:58 +0200

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install software-properties-common
+sudo apt install software-properties-common -y
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible -y

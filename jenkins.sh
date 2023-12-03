b7a3074324aa3192181988c4cb46af5ca6259002
oksesaneka22 <49028843+oksesaneka22@users.noreply.github.com>
2023-12-03 15:00:03 +0200

    Create jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt update
+sudo apt install default-jre
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+    https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+    https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install fontconfig openjdk-17-jre
+sudo apt-get install jenkins

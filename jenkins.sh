e775b40f116bb5439a5b3dd95699ee13f8c56ce4
oksesaneka22 <49028843+oksesaneka22@users.noreply.github.com>
2023-12-03 15:11:41 +0200

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -3,9 +3,7 @@ sudo apt update
 sudo apt install default-jre
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
     https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-    https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-    /etc/apt/sources.list.d/jenkins.list > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update -y
 sudo apt-get install fontconfig openjdk-17-jre
 sudo apt-get install jenkins

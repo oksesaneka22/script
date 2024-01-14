95b2ca3dc7296395162937dd22acc09846139d9b
oksesaneka22 <49028843+oksesaneka22@users.noreply.github.com>
2024-01-14 17:24:51 +0200

    Create grafana.sh

---
+++ grafana.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt update -y && sudo apt upgrade -y
+sudo apt install -y apt-transport-https software-properties-common wget
+sudo mkdir -p /etc/apt/keyrings/
+wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+sudo apt update -y
+sudo apt install grafana -y
+sudo grafana-server -v
+sudo systemctl start grafana-server
+sudo systemctl enable grafana-server
